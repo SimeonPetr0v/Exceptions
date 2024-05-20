03f29f791b1550c2f4005928cafca4948dd43699
SimeonPetr0v <simeonp925@gmail.com>
2024-05-20 20:46:38 +0300

    Homework

---
+++ Enter Numbers/Enter Numbers/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Enter_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+
+            List<int> numbers = new List<int>();
+
+            while (numbers.Count < 10)
+            {
+                start = ReadNumber(start, end);
+
+                if (start > 1)
+                {
+                    numbers.Add(start);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+
+
+            int ReadNumber(int start, int end)
+            {
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+
+                    if (number <= start || number >= end)
+                    {
+                        throw new Exception($"Your number is not in range {start} - {end}!");
+                    }
+
+                    return number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                return 1;
+            }
+        }
+    }
+}
---
+++ Square Root/Square Root/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n + 5);
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Sqrt(num));
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+            Console.WriteLine("Program ended");
+            }
+    }
+}
